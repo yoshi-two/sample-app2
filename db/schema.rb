aa7c043182c8d325532de424334ecd3d295ecf48
abe <z21041ta@aiit.ac.jp>
2021-04-30 10:29:18 +0000

    Validate password presence and length

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_102443) do
+ActiveRecord::Schema.define(version: 2021_04_30_100524) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
